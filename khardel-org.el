14a93d235ad9ffeb9f19bdc66c397a0a3bb8ff50
Damien Cassou <damien@cassou.me>
2019-11-24 12:36:21 +0100

    Release 0.2.0

--- khardel-org.el
+++ khardel-org.el
@@ -5,7 +5,7 @@
 ;; Author: Nicolas Petton <nicolas@petton.fr>
 ;; Url: https://gitlab.petton.fr/DamienCassou/khardel
 ;; Package-requires: ((emacs "26.1"))
-;; Version: 0.1.0
+;; Version: 0.2.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
